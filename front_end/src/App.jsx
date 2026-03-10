3a3d76cd94fc9bbce8f7b728fb9a3932cc1f90a6
Jade Dorsainvil <140849447+Jadeddor@users.noreply.github.com>
2026-03-09 21:53:40 -0400

    Update App.jsx

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -54,42 +54,7 @@ function App() {
 export default App;
 
 
-// import { useState } from "react";
-// import "./App.css";
-// import Login from "./pages/Login";
-// import ChatBot from "./pages/ChatBot";
-// import Admin from "./pages/FacultyDashboard";
-// import { Toaster } from "sonner";
 
-// function App() {
-//   const [user, setUser] = useState(null);
-
-//   const handleLogin = (userData) => {
-//     setUser(userData);
-//   };
-
-//   // If not logged in → show login
-//   if (!user) {
-//     return (
-//       <>
-//         <Login onLogin={handleLogin} />
-//         <Toaster />
-//       </>
-//     );
-//   }
-
-//   // Route by role:
-//   const isAdmin = user.role === "admin";
-//   const isChatUser = ["student", "faculty", "guest"].includes(user.role);
-
-//   return (
-//     <div className="App">
-//       {isAdmin && <Admin user={user} />}
-//       {isChatUser && <ChatBot user={user} />}
-
-//       <Toaster />
-//     </div>
-//   );
 // }
 
 // export default App;
